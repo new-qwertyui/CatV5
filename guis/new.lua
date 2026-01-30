85d8606142a1f4f452cfe4f26accf5c061914535
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2026-01-30 22:27:15 +0700

    Revert "Update new.lua"
    This reverts commit 600c35f81d3c15038e178ab24223b8a21e5eb410.

--- guis/new.lua
+++ guis/new.lua
@@ -61,10 +61,8 @@ LPH_NO_VIRTUALIZE(function()
 		tweenstwo = {}
 	}
 	local uipallet = {
-		--[[Main = Color3.fromRGB(26, 25, 26),
-		Text = Color3.fromRGB(200, 200, 200),]]
-		Main = Color3.fromRGB(220, 220, 220),
-		Text = Color3.fromRGB(60, 60, 60),
+		Main = Color3.fromRGB(26, 25, 26),
+		Text = Color3.fromRGB(200, 200, 200),
 		Font = Font.fromEnum(Enum.Font.Montserrat),
 		FontSemiBold = Font.fromEnum(Enum.Font.Montserrat, Enum.FontWeight.SemiBold),
 		Tween = TweenInfo.new(0.16, Enum.EasingStyle.Linear)
@@ -543,7 +541,7 @@ LPH_NO_VIRTUALIZE(function()
 		return getcustomasset('catrewrite/assets/new/minecraftfont.json')
 	end
 
-	--[[ pls replace laterdo
+	do
 		local res = isfile('catrewrite/profiles/color.txt') and loadJson('catrewrite/profiles/color.txt')
 		if res then 
 			uipallet.Main = res.Main and Color3.fromRGB(unpack(res.Main)) or uipallet.Main
@@ -555,7 +553,7 @@ LPH_NO_VIRTUALIZE(function()
 			uipallet.FontSemiBold = Font.new(uipallet.Font.Family, Enum.FontWeight.SemiBold)
 		end
 		fontsize.Font = uipallet.Font
-	end]]
+	end
 
 	do
 		function color.Dark(col, num)
