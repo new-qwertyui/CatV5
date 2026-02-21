57222ede0d69a05082be37dce9245cd8cb535fda
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2026-02-22 00:52:03 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -47,8 +47,8 @@ end
 local function wipeFolder(path)
 	if not isfolder(path) then return end
 	for _, file in listfiles(path) do
-		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+		if file:find('loader') or file:find('init') then continue end
+		if isfile(file) then
 			delfile(file)
 		end
 	end
