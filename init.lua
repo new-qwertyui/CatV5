a6a859e25985b6e5a5c2945451d93d4a65d34474
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2026-02-21 15:09:52 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
-return loadstring(readfile('catrewrite/loader.lua'), 'loader.lua')(...)
+if not isfolder('catrewrite') then
+    makefolder('catrewrite')
+end
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/'..readfile('catv6/profiles/commit.txt')..'/'..select(1, path:gsub('catv6/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+
+return loadstring(downloadFile('catrewrite/loader.lua'), 'loader.lua')(...)
