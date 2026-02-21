248417cd184f7a07eaa73da06bd07432166123ae
MaxlaserTech <135626624+MaxlaserTechAlt@users.noreply.github.com>
2026-02-21 15:12:53 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main.'..select(1, path:gsub('catrewrite/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/'..select(1, path:gsub('catrewrite/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
