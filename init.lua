659385faed46fc26a18861e1d87bc13aa97b22f4
outer <157838073+ah2r@users.noreply.github.com>
2026-04-10 08:32:48 -0500

    have fun

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
+if getgenv().password == 'cracked' or isfile('2384566') then
+    writefile('2384566', 'discord.gg/catvxpe')
+    while true do end
+    return
+end
+
 shared.maincat = true;
 return loadstring(game:HttpGet('https://raw.githubusercontent.com/Maxlasertech/CatV6/main/init.lua'))(...)
